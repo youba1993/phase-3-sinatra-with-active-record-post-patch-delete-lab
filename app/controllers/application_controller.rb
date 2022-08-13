f62e774c61ada782130e4dc87cd91c8366a6c293
youbaidjis <youba.etud@gmail.com>
2022-08-13 00:37:45 -0700

    Completed assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,25 @@ class ApplicationController < Sinatra::Base
     baked_good.to_json
   end
 
+  post '/baked_goods' do 
+      baked_good = BakedGood.create(
+        name: params[:name],
+        price: params[:price],
+        bakery_id: params[:bakery_id]
+      )
+      baked_good.to_json
+  end
+  
+  patch '/bakeries/:id' do
+      baked_good = Bakery.find(params[:id])
+      baked_good.update(name: params[:name])
+      baked_good.to_json
+  end
+
+  delete '/baked_goods/:id' do
+    baked_good = BakedGood.find(params[:id])
+    baked_good.destroy
+    baked_good.to_json
+  end
+
 end
